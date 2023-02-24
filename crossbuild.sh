93ca696a3748d30eba57d25f3b753fd9fa5d9ff0
wwqgtxx <wwqgtxx@gmail.com>
2023-02-24 14:43:13 +0800

    instruct gox to build statically linked binaries

--- crossbuild.sh
+++ crossbuild.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# instruct gox to build statically linked binaries
+export CGO_ENABLED=0
+
 mkdir binaries
 gox -os="windows darwin linux" -arch="amd64 386 arm arm64" -osarch="!darwin/arm !darwin/386" -output "binaries/{{.Dir}}-{{.OS}}-{{.Arch}}"
 cp config.yaml binaries/
